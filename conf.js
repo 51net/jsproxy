2b45740dff198f203c5cd2e37328722431d9475e
51net <66149523+51net@users.noreply.github.com>
2021-10-15 07:41:10 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '113',
+  ver: '100',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -41,7 +41,7 @@ jsproxy_config({
     },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
-      label: 'cfworker',
+      label: '',
       hidden: true,
       lines: {
         // 收费版（高权重）
@@ -96,7 +96,7 @@ jsproxy_config({
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
     'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
+      redir: 'https://itprosec.com/'
     },
     'http://haha.com/': {
       content: 'Hello World'
