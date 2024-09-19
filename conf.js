e0f30bbe4f86d35f809675240a654ca3fa140d4c
51net <66149523+51net@users.noreply.github.com>
2024-09-19 08:05:35 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -95,11 +95,14 @@ jsproxy_config({
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
-    'https://www.pornhub.com/': {
+    'https://www.pornhub.com/*': {
       redir: 'https://itprosec.com/'
     },
     'http://haha.com/': {
       content: 'Hello World'
+    },
+    'https://ero-comic-hunter.net': {
+      redir: 'https://itprosec.com/'
     },
   }
 })
